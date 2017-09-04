0ec1ffb10f3b03372e5b7d0e8df41118c32c0a4f
LucianoKlein <kleinersilver@gmail.com>
2017-09-05 02:15:53 +0800

    "practice spring with maven"

---
+++ src/test/java/test/Demo.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.hexin.pojo.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Demo {
+	@Test
+	public void test1() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		User user = (User) ac.getBean("user");
+		//System.out.println(user.getName());
+	}
+}
